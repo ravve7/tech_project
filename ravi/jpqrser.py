b2303b148fea137693f4192de48722948baaaf78
ravikumarmaliya <ravikumarm062s4@gmail.com>
2019-07-30 16:10:00 +0200

    final_edits

--- ravi/jpqrser.py
+++ ravi/jpqrser.py
@@ -1,29 +1,13 @@
 import requests
 import json
-
 from pytz import utc
-
-
 def all_data():
     response = requests.get('http://127.0.0.1:8000/api/inventory_api/')
     data = response.json()
-
     myDict = dict()
     for each in data:
         name=each['products']['product_name']
         date=each['date']
         count = each['inventory_level']
         myDict.setdefault(name, []).append([date,count])
-
     return myDict
-
-#
-# from datetime import datetime
-# from dateutil import tz
-# import dateutil.parser
-# # METHOD 1: Hardcode zones:
-# format = '%Y-%m-%dT%H:%M:%S%z'
-# datestring = '2019-07-29T15:12:45-02:00'
-# d = dateutil.parser.parse(datestring) # python 2.7
-# d = d.replace() - d.utcoffset()
-# print(d)
--- ravi/views.py
+++ ravi/views.py
@@ -11,20 +11,15 @@ from  datetime import datetime
 from . import jpqrser
 import requests
 
-
-
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Products.objects.all()
     serializer_class = ProductSerializer
-
 
 class inventoryViewSet(viewsets.ModelViewSet):
     queryset = inventory.objects.all()
     serializer_class = inventorySerializer
 
-
 def HomePageView(request):
-
     context = {}
     context['form'] = ProductForm()
     context['products']=[]
@@ -32,7 +27,6 @@ def HomePageView(request):
     for each in data:
         p_name=each
         for dt in data[p_name]:
-            print("---->"+each)
             p_date=datetime.strptime(str(dt[0][:24]).split(".")[0], "%Y-%m-%dT%H:%M:%S")
             p_inv=dt[1]
             l_data= dict()
@@ -40,40 +34,13 @@ def HomePageView(request):
             l_data.setdefault('date', (p_date).strftime("%d-%m-%Y %H:%M:%S"))
             l_data.setdefault('inv', p_inv)
             context['products'].append(l_data)
-
-
-
-    # context['products'] = Products.objects.values(
-    #     'id', 'product_name', 'inventory__date', 'inventory__inventory_level')
-    for i in context['products']:
-        print(i['name'])
-    inv=[]
-    date=[]
-
+    inv=[],date=[]
     if request.method == "POST":
-
         form = ProductForm(request.POST)
-
         if form.is_valid():
-
             prod_name=str(form.cleaned_data["selectproduct"])
-
-
-            # id_of_product = (Products.objects.filter(product_name=str(
-            #     form.cleaned_data["selectproduct"])).values_list('pk', flat=True)[0])
-            #
-            # name = Products.objects.filter(pk=id_of_product).values()[
-            #     0]['product_name']
-            # context['prod_name'] = name
-            #
-            # for h in inventory.objects.filter(
-            #         products_id=id_of_product):
-            #     date.append((h.date).strftime("%d-%m-%Y %H:%M:%S"))
-            #     inv.append(h.inventory_level)
-
             getData=data[prod_name]
             context['products'] = []
-            print(getData)
             for dt in getData:
                 date.append(datetime.strptime(str(dt[0][:24]).split(".")[0], "%Y-%m-%dT%H:%M:%S"))
                 inv.append(dt[1])
@@ -84,31 +51,16 @@ def HomePageView(request):
                 l_data.setdefault('date', datetime.strptime(str(dt[0][:24]).split(".")[0], "%Y-%m-%dT%H:%M:%S"))
                 l_data.setdefault('inv', dt[1])
                 context['products'].append(l_data)
-
-            trace1 = go.Scatter(
-                x=date,
-                y=inv
-            )
-
+            
+            trace1 = go.Scatter(x=date,y=inv)
             data = [trace1]
-            layout = go.Layout(
-
-                width=900,
-                height=600,
+            layout = go.Layout(width=900,height=600,
                 #title=go.layout.Title(text="chart : {} ".format(prod_name)),
-
-                xaxis=dict(
-                    autorange=True
-                ),
-                yaxis=dict(
-                    autorange=True
-                )
-            )
+                xaxis=dict(autorange=True),
+                yaxis=dict(autorange=True))
             fig = go.Figure(data=data, layout=layout)
             fig.update_xaxes(title_text='Date')
             fig.update_yaxes(title_text='Inventory_level')
             plot_div = plot(fig, output_type='div', include_plotlyjs=False)
             context['plot'] = plot_div
-
-
     return render(request, 'HomePage.html', context)
